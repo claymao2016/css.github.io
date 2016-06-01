c7c1628ebc04286bc4e291c9f1c7de3e1d304fd0
claymao2016 <1361163644@qq.com>
2016-06-01 12:38:54 +0800

    Javascrpt再练习

---
+++ Javascript练习/过滤器/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<style type="text/css" media="screen">
+	<title>过滤器</title>
+</head>
+<body>
+
+</body>
+</html>
